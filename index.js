c910665a26be976242821207d96ff74a31828b9b
phi <phi1618jp@gmail.com>
2015-10-05 15:27:04 +0900

    setup

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require("express");
+var path = require('path');
 var app = express();
-var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/"))
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var server = app.listen(port, function() {
-  console.log('start');
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Home' });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
 });
