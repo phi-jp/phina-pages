3ba550c2eed422bafbc46d4e2481190b745412dd
phi <phi1618jp@gmail.com>
2015-11-26 17:29:23 +0900

    remove file

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.render('index', { title: 'Home' });
 });
-app.get('/about', function(req, res) {
-  res.render('about', { title: 'About' });
-});
 
 app.listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
