c02a1c3f9eba40209d465ea1fc9b3ea3f24c5e4a
phi <phi1618jp@gmail.com>
2015-11-28 20:57:09 +0900

    fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var express = require("express");
+var fs = require('fs');
 var path = require('path');
+var express = require("express");
+var zip = require("express-zip");
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -9,8 +11,28 @@ app.set('view engine', 'jade');
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+var template = (function() {
+  var html = fs.readFileSync('template/index.html').toString();
+  var js = fs.readFileSync('template/main.js').toString();
+
+  return {
+    html: html,
+    js: js,
+  };
+})();
+
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Home' });
+  res.render('index', {
+    title: 'Home',
+    template: template,
+  });
+});
+
+app.get('/download', function(req, res) {
+  res.zip([
+    { path: 'template/index.html', name: 'phinajs/index.html', },
+    { path: 'template/main.js', name: 'phinajs/main.js', },
+  ], 'phinajs.zip');
 });
 
 app.listen(app.get('port'), function() {
