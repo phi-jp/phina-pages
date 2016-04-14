ad9b2bc1a77067b57ab03ddc53bfe28c206895b4
phi <phi1618jp@gmail.com>
2016-04-15 02:42:52 +0900

    add docframe

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var shell = require('gulp-shell');
 
 
 gulp.task('docs', shell.task([
-  'jsduck ../phina.js/src --output ./public/docs --title "phina.js docs"',
+  'jsduck ../phina.js/src --output ./public/docs --title "phina.js docs" --eg-iframe=docframe.html',
 ]));
